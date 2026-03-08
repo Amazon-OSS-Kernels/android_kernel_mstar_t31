2a2629dadd089849eaddb1efb7c2f232f50edaf5
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 08:54:31 -0500

    JVC_UHD_FireTV_2021-7.6.9.7-20250513 [bootable]

--- bootable/bootloader/ufbl-features/project/mtk_t31.mk
+++ bootable/bootloader/ufbl-features/project/mtk_t31.mk
@@ -19,12 +19,12 @@ UFBL_PLAT_MTK_T31 := yes
 export UFBL_PLAT_MTK_T31
 DEFINES += -DUFBL_PLAT_MTK_T31
 
-# for specific changes per project; skipper reuses brandenburg
+# for specific changes per project; ABC reuses brandenburg
 ifeq ($(TARGET_PRODUCT), duckie)
 DEFINES += -DUFBL_PROJ_DUCKIE
 else ifeq ($(TARGET_PRODUCT), brandenburg)
 DEFINES += -DUFBL_PROJ_BRANDENBURG
-else ifeq ($(TARGET_PRODUCT), skipper)
+else ifeq ($(TARGET_PRODUCT), ABC)
 DEFINES += -DUFBL_PROJ_BRANDENBURG
 else ifeq ($(TARGET_PRODUCT), anna)
 DEFINES += -DUFBL_PROJ_ANNA
