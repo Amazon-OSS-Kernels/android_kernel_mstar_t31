c5cfbe7fbf6c1fd9524024d44fa5ecfad957beb6
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 09:47:32 -0500

    OK_UHD_FireTV_2021-7.6.9.7-20250514 [bootable]

--- bootable/bootloader/uboot-mstar/t31/u-boot-2011.06/MstarCore/src/drivers/demura/mstar/URSA/M7632/demura_common.h
+++ bootable/bootloader/uboot-mstar/t31/u-boot-2011.06/MstarCore/src/drivers/demura/mstar/URSA/M7632/demura_common.h
@@ -51,23 +51,31 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *****************************************************************************/
+#ifndef _DEMURA_COMMON_H_
+#define _DEMURA_COMMON_H_
 
-#ifndef _MS_API_MSPI_
-#define _MS_API_MSPI_
+// Function declare
+int demura_layer_distance_div(int pre, int nxt);
+int register_shift(int mask, int value);
+void part_bit15_00(int input_value, int total_bits, int *Bit15_08, int *Bit07_00);
+void part_bit23_00(int input_value, int total_bits, int *Bit23_16, int *Bit15_08, int *Bit07_00);
+void demura_table_normalization(int lut_size, double *lut_in, int *lut_out,int *layer_gain_out);
+void demura_table_normalization1(int lut_size, double *lut_in, int *lut_out, int *layer_gain_out);
+void demura_table_normalization_ROI(int lut_h_size, int lut_v_size, int ROI_h_start_offset, int ROI_h_end_offset, int ROI_v_start_offset, int ROI_v_end_offset, double *lut_in, int *lut_out, int *layer_gain_out);
+void demura_table_normalization_inv(int lut_size, int layer_gain,int *lut_in,double *lut_out);
+void coeff_and_ks(int diff, int *coeff, int *ks);
+void FIND_MIN_AND_MAX(int RGB_channel_idx, int block_width, int block_height, strgb_structInfo *layer_blk_img, double *rgb_max, double *rgb_min);
+void LUT_SETTING_SUBFUNCTION(int RGB_idx, int plane_idx, int block_width, int block_height, int reg_dmc_plane_level, registers *preg, strgb_structInfo *layer_blk_img, strgb_structInfo *layer_blk_img_out);
+void LUT_AND_SETTING_PROCESSOR( int plane_idx, int block_width, int block_height, int reg_dmc_plane_level, registers *preg , strgb_structInfo *layer_blk_img_in, strgb_structInfo *layer_blk_img_out);
+void write_demuraLUT_to_HW_DRAM_FORMAT(stlayer_info_structInfo *layer_info, strgb_structInfo *layer_blk_img[8], m_vector *pLut_out_vector);
 
-#include <MsTypes.h>
+int GETBITS( const int Value , const int MSB_bit, const int LSB_bit);
+int register_08bits_inv( _08bits reg_tmp, int total_bits);
+int register_16bits_inv( _16bits reg_tmp, int MSB_total_bits, int LSB_total_bits);
+int twos_complement_to_integer(const int input_value, const int total_bits);
+double DMC_LUT_Fix_To_Double(const int Layer_offset_2s, const int Layer_gain, const int Lut_value);
 
-#if (ENABLE_CMD_SF == 1)
-int MDrv_MSPI_CFG_PIN(int port_num);
+// extern function
+extern MS_BOOL demura_core_ver10(int base_address, int lut_h_size, int lut_v_size, interface_info *pInfo, m_vector *pRegVector, m_vector *Lut_out);
 
-#if defined(CONFIG_MSPI_CS_GPIO)
-void MDrv_MSPI_CFG_CS(MS_BOOL Enable);
 #endif
-#endif
-
-void MDrv_MSPI_DEMURA_CS_OUT(int Enable);
-
-int do_dolby_init(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
-
-#endif
-
--- bootable/bootloader/uboot-mstar/t31/u-boot-2011.06/MstarCore/src/drivers/demura/mstar/vendor/csot_csot/vendor.h
+++ bootable/bootloader/uboot-mstar/t31/u-boot-2011.06/MstarCore/src/drivers/demura/mstar/vendor/csot_csot/vendor.h
@@ -51,23 +51,67 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  *****************************************************************************/
+#ifndef _VENDOR_H_
+#define _VENDOR_H_
 
-#ifndef _MS_API_MSPI_
-#define _MS_API_MSPI_
+#define HEADER_START   0
+#define HEADER_LEN     0x110
+#define LUT_START      0x110
+#define CSOT_CSOT_DATA_BIT_NUM 12
 
-#include <MsTypes.h>
+typedef enum
+{
+    E_MONO_MODE  = 0,
+    E_RGB_MODE   = 1,
+} E_COLOR_MODE;
 
-#if (ENABLE_CMD_SF == 1)
-int MDrv_MSPI_CFG_PIN(int port_num);
+typedef enum
+{
+    E_FILE_TATOL_CRC = 0,
+    E_FILE_HEADER_CRC,
+    E_DEMURA_PARA_CRC,
+    E_DEMURA_LUT_CRC,
+} E_FILE_CRC;
 
-#if defined(CONFIG_MSPI_CS_GPIO)
-void MDrv_MSPI_CFG_CS(MS_BOOL Enable);
+typedef struct
+{
+    MS_U8           DEMURA_PLANE_NUM;
+    MS_BOOL         DEMURA_MODE;
+    MS_U16          DEMURA_TBL_H;
+    MS_U16          DEMURA_TBL_V;
+    MS_U8           DEMURA_BLK_H;
+    MS_U8           DEMURA_BLK_V;
+    MS_U32          TABLE_CRC;
+    MS_U16          DEMURA_BLACK_LIMIT;
+    MS_U16          DEMURA_PLANE_LEVEL1;
+    MS_U16          DEMURA_PLANE_LEVEL2;
+    MS_U16          DEMURA_PLANE_LEVEL3;
+    MS_U16          DEMURA_PLANE_LEVEL4;
+    MS_U16          DEMURA_PLANE_LEVEL5;
+    MS_U16          DEMURA_PLANE_LEVEL6;
+    MS_U16          DEMURA_PLANE_LEVEL7;
+    MS_U16          DEMURA_PLANE_LEVEL8;
+    MS_U16          DEMURA_WHITE_LIMIT;
+    MS_U16          DEMURA_GAIN_R;
+    MS_U16          DEMURA_GAIN_G;
+    MS_U16          DEMURA_GAIN_B;
+    MS_U16          DEMURA_OFFSET_R;
+    MS_U16          DEMURA_OFFSET_G;
+    MS_U16          DEMURA_OFFSET_B;
+    MS_U8           DEMURA_INT_BIT_WIDTH;
+    MS_U8           DEMURA_DEC_BIT_WIDTH;
+    // Header Buffer
+    MS_U8           *hdr_buffer;
+    // Lut Buffer
+    MS_U8           *lut_buffer;
+    MS_U32          lut_offset;
+    MS_U32          lut_length;
+}CSOT_CSOT_Demura_Header;
+
+MS_U16 get_CSOT_CSOT_CRC(E_FILE_CRC eCRCType);
+MS_BOOL load_vendor_header(CSOT_CSOT_Demura_Header *phdr);
+MS_BOOL parse_vendor_header(CSOT_CSOT_Demura_Header *phdr);
+void dump_vendor_header(CSOT_CSOT_Demura_Header *phdr);
+MS_BOOL load_vendor_lut(CSOT_CSOT_Demura_Header *phdr);
+
 #endif
-#endif
-
-void MDrv_MSPI_DEMURA_CS_OUT(int Enable);
-
-int do_dolby_init(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]);
-
-#endif
-
