c89de10287fa88aaacbf95331297cbd3840875e0
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 07:47:51 -0500

    JVC_UHD_FireTV_2021-7.6.4.6-20230601 [vendor]

--- vendor/amazon/wlan/mediatek/driver/mt76x8/mgmt/ais_fsm.c
+++ vendor/amazon/wlan/mediatek/driver/mt76x8/mgmt/ais_fsm.c
@@ -1084,7 +1084,9 @@ VOID aisFsmSteps(IN P_ADAPTER_T prAdapter, ENUM_AIS_STATE_T eNextState)
 									cnmTimerStopTimer(prAdapter,
 										&prAdapter->rWifiVar.rDBDCDisableCountdownTimer);
 
-								if (timerPendingTimer(&prAdapter->rWifiVar.rDBDCSwitchGuardTimer))
+								/* only stop pening Switch Guard Timer when DBDC is being disabled */
+								if (timerPendingTimer(&prAdapter->rWifiVar.rDBDCSwitchGuardTimer) &&
+									!prAdapter->rWifiVar.fgDbDcModeEn)
 									cnmTimerStopTimer(prAdapter,
 										&prAdapter->rWifiVar.rDBDCSwitchGuardTimer);
 
@@ -2283,6 +2285,8 @@ enum _ENUM_AIS_STATE_T aisFsmJoinCompleteAction(IN struct _ADAPTER_T *prAdapter,
 				}
 			}
 		}
+		DBGLOG(AIS, STATE, "Joined BSS eBand %d channel %d ucChannelBw %d\n", prAisBssInfo->eBand,
+                       prAisBssInfo->ucPrimaryChannel, rlmDomainGetChannelBw(prAisBssInfo->ucPrimaryChannel));
 	return eNextState;
 }
 
@@ -4006,7 +4010,7 @@ aisDeauthXmitComplete(IN P_ADAPTER_T prAdapter, IN P_MSDU_INFO_T prMsduInfo, IN 
 	ASSERT(prAdapter);
 
 	prAisFsmInfo = &(prAdapter->rWifiVar.rAisFsmInfo);
-	if (rTxDoneStatus == TX_RESULT_SUCCESS)
+	if (rTxDoneStatus == TX_RESULT_SUCCESS || rTxDoneStatus == TX_RESULT_DROPPED_IN_DRIVER)
 		cnmTimerStopTimer(prAdapter, &prAisFsmInfo->rDeauthDoneTimer);
 
 	if (prAisFsmInfo->eCurrentState == AIS_STATE_DISCONNECTING) {
