84e35aca110fd34861a5747d874b85d2d811522a
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 00:41:59 -0600

    AmazonFireTVEdition_4K_UHD-7.6.5.2-20231107 [bootable]

--- bootable/bootloader/ufbl-features/project/mtk_t31.mk
+++ bootable/bootloader/ufbl-features/project/mtk_t31.mk
@@ -28,8 +28,8 @@ else ifeq ($(TARGET_PRODUCT), skipper)
 DEFINES += -DUFBL_PROJ_BRANDENBURG
 else ifeq ($(TARGET_PRODUCT), anna)
 DEFINES += -DUFBL_PROJ_ANNA
-else ifeq ($(TARGET_PRODUCT), ABC)
-DEFINES += -DUFBL_PROJ_ABC
+else ifeq ($(TARGET_PRODUCT), teddy)
+DEFINES += -DUFBL_PROJ_TEDDY
 else ifeq ($(TARGET_PRODUCT), hailey)
 DEFINES += -DUFBL_PROJ_HAILEY
 else ifeq ($(TARGET_PRODUCT), juliana)
