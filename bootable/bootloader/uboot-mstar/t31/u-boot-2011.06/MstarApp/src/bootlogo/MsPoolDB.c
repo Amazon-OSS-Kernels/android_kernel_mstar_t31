4bfc78894f04cf9aff25f71ca6d75bdcc30dcf9d
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 13:51:01 -0500

    FireTVOmniSeries_4K_UHD-7.2.9.2-20231017 [bootable]

--- bootable/bootloader/uboot-mstar/t31/u-boot-2011.06/MstarApp/src/bootlogo/MsPoolDB.c
+++ bootable/bootloader/uboot-mstar/t31/u-boot-2011.06/MstarApp/src/bootlogo/MsPoolDB.c
@@ -5872,7 +5872,7 @@ int Load_LDMPara_ToFlash(U32 u32DbtableOffset)
                 UBOOT_INFO("set boot env ldm_led_device_name to be APE5030.\n");
                 setenv("ldm_led_device_name", "ape5030");
 
-                snprintf(strApe5030_ch_map, sizeof(strApe5030_ch_map), "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X",
+                snprintf(strApe5030_ch_map, sizeof(strApe5030_ch_map), "%x:%x:%x:%x:%x:%x:%x:%x:%x:%x:%x:%x",
                         device_info.u16LED_CMD_CUR_ON_1[0], device_info.u16LED_CMD_CUR_ON_1[1], device_info.u16LED_CMD_CUR_ON_1[2], device_info.u16LED_CMD_CUR_ON_1[3],
                         device_info.u16LED_CMD_CUR_ON_1[4], device_info.u16LED_CMD_CUR_ON_1[5], \
                         device_info.u16LED_CMD_CUR_ON_2[0], device_info.u16LED_CMD_CUR_ON_2[1], \
