21867337fff124d679f992b83a430c31fed93098
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 00:24:12 -0600

    AmazonFireTVEdition_4K_UHD-7.6.5.2-20231107 [vendor]

--- vendor/amazon/wlan/mediatek/driver/mt76x8/os/linux/include/gl_kal.h
+++ vendor/amazon/wlan/mediatek/driver/mt76x8/os/linux/include/gl_kal.h
@@ -235,7 +235,6 @@ typedef enum _ENUM_SPIN_LOCK_CATEGORY_E {
 	SPIN_LOCK_EHPI_BUS,	/* only for EHPI */
 	SPIN_LOCK_NET_DEV,
 	SPIN_LOCK_CHIP_RST,
-	SPIN_LOCK_PROC_FS,
 #ifdef CFG_SUPPORT_MULTICAST_ENHANCEMENT_LOOKBACK
 	SPIN_LOCK_TX_LB_QUE,
 	SPIN_LOCK_RX_LB_QUE,
@@ -627,6 +626,14 @@ static inline void kalCfg80211ScanDone(struct cfg80211_scan_request *request,
 })
 #endif
 
+#define kalMemZAlloc(u4size, eMemType) ({    \
+	void *pvAddr; \
+	pvAddr = kalMemAlloc(u4size, eMemType);   \
+	if (pvAddr) \
+		kalMemSet(pvAddr, 0, u4size);   \
+	pvAddr; \
+})
+
 /*----------------------------------------------------------------------------*/
 /*!
 * \brief Free allocated cache memory
@@ -942,7 +949,11 @@ kalDevPortWrite(P_GLUE_INFO_T prGlueInfo,
 		IN UINT_16 u2Port, IN UINT_32 u2Len, IN PUINT_8 pucBuf, IN UINT_32 u2ValidInBufSize);
 
 BOOL kalDevWriteData(IN P_GLUE_INFO_T prGlueInfo, IN P_MSDU_INFO_T prMsduInfo);
+#if CFG_FTV_62866_PATCH
+WLAN_STATUS kalDevWriteCmd(IN P_GLUE_INFO_T prGlueInfo, IN P_CMD_INFO_T prCmdInfo, IN UINT_8 ucTC);
+#else
 BOOL kalDevWriteCmd(IN P_GLUE_INFO_T prGlueInfo, IN P_CMD_INFO_T prCmdInfo, IN UINT_8 ucTC);
+#endif
 BOOL kalDevKickData(IN P_GLUE_INFO_T prGlueInfo);
 VOID kalDevReadIntStatus(IN P_ADAPTER_T prAdapter, OUT PUINT_32 pu4IntStatus);
 
