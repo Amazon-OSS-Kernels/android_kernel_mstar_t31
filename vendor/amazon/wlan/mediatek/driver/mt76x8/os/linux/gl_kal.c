b51d2a5e8190bc1447e34838dcb5d6e557ce2814
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 07:26:58 -0500

    Hisense_QLED_FireTVEdition_4K_UHD-7.6.8.5-20250108 [vendor]

--- vendor/amazon/wlan/mediatek/driver/mt76x8/os/linux/gl_kal.c
+++ vendor/amazon/wlan/mediatek/driver/mt76x8/os/linux/gl_kal.c
@@ -1016,7 +1016,7 @@ VOID
 kalIndicateStatusAndComplete(IN P_GLUE_INFO_T prGlueInfo, IN WLAN_STATUS eStatus, IN PVOID pvBuf, IN UINT_32 u4BufLen)
 {
 
-	UINT_32 bufLen;
+	UINT_32 bufLen = 0;
 	P_PARAM_STATUS_INDICATION_T pStatus = (P_PARAM_STATUS_INDICATION_T) pvBuf;
 	P_PARAM_AUTH_EVENT_T pAuth = (P_PARAM_AUTH_EVENT_T) pStatus;
 	P_PARAM_PMKID_CANDIDATE_LIST_T pPmkid = (P_PARAM_PMKID_CANDIDATE_LIST_T) (pStatus + 1);
@@ -5375,7 +5375,7 @@ static ssize_t kalMetPortWriteProcfs(struct file *file, const char __user *buffe
 {
 	char acBuf[128 + 1];	/* + 1 for "\0" */
 	UINT_32 u4CopySize;
-	int u16MetUdpPort;
+	int u16MetUdpPort = 0;
 
 	IN P_GLUE_INFO_T prGlueInfo;
 
@@ -5750,6 +5750,10 @@ VOID kalFreeTxMsduWorker(struct work_struct *work)
 
 	while (QUEUE_IS_NOT_EMPTY(prTmpQue)) {
 		QUEUE_REMOVE_HEAD(prTmpQue, prMsduInfo, P_MSDU_INFO_T);
+		if (!prMsduInfo) {
+			DBGLOG(REQ, WARN, "prMsduInfo is NULL\n");
+			break;
+		}
 
 		nicTxFreePacket(prAdapter, prMsduInfo, FALSE);
 		nicTxReturnMsduInfo(prAdapter, prMsduInfo);
@@ -5769,7 +5773,7 @@ VOID kalFreeTxMsdu(P_ADAPTER_T prAdapter, P_MSDU_INFO_T prMsduInfo)
 #if CFG_SUPPORT_DFS
 VOID kalIndicateChannelSwitch(IN P_GLUE_INFO_T prGlueInfo, IN ENUM_CHNL_EXT_T eSco, IN UINT_8 ucChannelNum)
 {
-	struct cfg80211_chan_def chandef;
+	struct cfg80211_chan_def chandef = {0};
 	struct ieee80211_channel *prChannel = NULL;
 	enum nl80211_channel_type rChannelType;
 
